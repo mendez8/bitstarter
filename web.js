c48a983e2b459f190db4859fdb8ebee06fda105e
Miguel Mendez <mendez8@gmail.com>
2013-07-06 23:18:27 +0100

    web.js to read from index.html

--- web.js
+++ web.js
@@ -3,8 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Gordi!');
+  response.send(fs.readFileSync('./index.html','utf8'));
 });
+
+//var k = fs.readFileSync('./index.html','utf8');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
