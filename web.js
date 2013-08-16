ef73cb27283e1b3f14d91058f9db29fdbd0df125
mendez8 <mendez8@gmail.com>
2013-08-16 13:20:41 +0100

    web.js and index.html minor updates

--- web.js
+++ web.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
-
 var express = require('express');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('./index.html','utf8'));
+  response.send(fs.readFileSync(htmlfile,'utf8'));
 });
 
 //var k = fs.readFileSync('./index.html','utf8');
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
